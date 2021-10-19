23376d68a29afe96cf86d1a51dd56689709ac8ce
Stephen Gutekanst <stephen@sourcegraph.com>
2021-10-19 07:36:09 -0700

    WIP querying
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/internal/codeintel/stores/lsifstore/documentation.go
+++ enterprise/internal/codeintel/stores/lsifstore/documentation.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/cockroachdb/errors"
 	"github.com/keegancsmith/sqlf"
@@ -354,4 +355,226 @@ func (s *Store) documentationDefinitions(
 	}
 
 	return locations, totalCount, nil
+}
+
+// DocumentationSearch searches API documentation in either the "public" or "private" table.
+//
+// 🚨 SECURITY: If the input tableSuffix is "private", then it is the callers responsibility to
+// enforce that the returned results are from repositories the user has permission to.
+func (s *Store) DocumentationSearch(ctx context.Context, tableSuffix, query string, repos []string) (_ []precise.DocumentationSearchResult, err error) {
+	ctx, _, endObservation := s.operations.documentationSearch.WithAndLogger(ctx, &err, observation.Args{LogFields: []log.Field{
+		log.String("query", query),
+		log.String("repos", fmt.Sprint(repos)),
+	}})
+	defer endObservation(1, observation.Args{})
+
+	// First, we split the query at the first colon if there is one. We support two syntaxes:
+	//
+	// 	<metadata>:<query terms>
+	// 	<query terms and metadata>
+	//
+	// That is, if the user jumbling all the query terms and metadata (repo names, tags, language name, etc.)
+	// into the query does not yield good results, they can prefix their query with metadata and a colon, for
+	// queries like:
+	//
+	// 	golang/go: net/http
+	// 	go package: mux
+	// 	go private variable my/repo: mux
+	//
+	// This is a stupid-simple form for people to get more specific results.
+	metaQuery := query
+	mainQuery := query
+	if i := strings.Index(query, ":"); i != -1 {
+		metaQuery = strings.TrimSpace(query[:i])
+		mainQuery = strings.TrimSpace(query[i+len(":"):])
+	}
+
+	// whether or not lexemes should match substrings. e.g. should "gith.com/sourcegraph/source"
+	// be matched as "*gith*.*com*/*sourcegraph*/*source*".
+	subStringMatches := true
+	langRepoTagsClause := textSearchQuery("tsv", metaQuery, subStringMatches)
+
+	var primaryClauses []*sqlf.Query
+	primaryClauses = append(primaryClauses, textSearchQuery("search_key_tsv", mainQuery, subStringMatches))
+	primaryClauses = append(primaryClauses, textSearchQuery("search_key_reverse_tsv", reverse(mainQuery), subStringMatches))
+	primaryClauses = append(primaryClauses, textSearchQuery("label_tsv", mainQuery, subStringMatches))
+	primaryClauses = append(primaryClauses, textSearchQuery("label_reverse_tsv", reverse(mainQuery), subStringMatches))
+
+	return s.scanDocumentationSearchResults(s.Store.Query(ctx, sqlf.Sprintf(
+		strings.ReplaceAll(documentationSearchQuery, "$SUFFIX", tableSuffix),
+		langRepoTagsClause, // matching_lang_names CTE WHERE conditions
+		langRepoTagsClause, // matching_repo_names CTE WHERE conditions
+		langRepoTagsClause, // matching_tags CTE WHERE conditions
+
+		textSearchRank("search_key_tsv", mainQuery, subStringMatches),                  // search_key_rank
+		textSearchRank("search_key_reverse_tsv", reverse(mainQuery), subStringMatches), // search_key_reverse_rank
+		textSearchRank("label_tsv", mainQuery, subStringMatches),                       // label_rank
+		textSearchRank("label_reverse_tsv", reverse(mainQuery), subStringMatches),      // label_reverse_rank
+
+		sqlf.Join(primaryClauses, " OR "), // primary WHERE clause
+		50,                                // result limit
+	)))
+}
+
+func textSearchRank(columnName, query string, subStringMatches bool) *sqlf.Query {
+	var rankFunctions []*sqlf.Query
+	for _, term := range strings.Fields(query) {
+		seq := lexemeSequence(term, subStringMatches)
+		rankFunctions = append(rankFunctions, sqlf.Sprintf("ts_rank_cd("+columnName+", %s, 2)", seq))
+	}
+	return sqlf.Join(rankFunctions, " + ")
+}
+
+// Note: This composing a tsquery _value_ and so using fmt.Sprintf instead of sqlf.Sprintf is
+// correct here.
+func lexemeSequence(s string, subStringmatches bool) string {
+	lexemes := lexemes(s)
+	sequence := make([]string, 0, len(lexemes))
+	for _, lexeme := range lexemes {
+		if subStringmatches {
+			sequence = append(sequence, fmt.Sprintf("%s:*", lexeme))
+		} else {
+			sequence = append(sequence, lexeme)
+		}
+	}
+	return strings.Join(sequence, " <-> ")
+}
+
+func textSearchQuery(columnName, query string, subStringMatches bool) *sqlf.Query {
+	var expressions []*sqlf.Query
+	for _, term := range strings.Fields(query) {
+		seq := lexemeSequence(term, subStringMatches)
+		expressions = append(expressions, sqlf.Sprintf(columnName+" @@ %s", seq))
+	}
+	return sqlf.Join(expressions, " OR ")
+}
+
+const documentationSearchQuery = `
+-- source: enterprise/internal/codeintel/stores/lsifstore/documentation.go:DocumentationSearch
+SELECT
+	id,
+	repo_id,
+	path_id,
+	detail,
+	search_key,
+	label,
+	lang_name,
+	repo_name,
+	tags,
+	search_key_rank,
+	search_key_reverse_rank,
+	label_rank,
+	label_reverse_rank
+FROM (
+    WITH
+        matching_lang_names AS (
+            SELECT id, lang_name
+            FROM lsif_data_docs_search_lang_names_$SUFFIX
+            WHERE %s LIMIT 1
+        ),
+        matching_repo_names AS (
+            SELECT id, repo_name
+            FROM lsif_data_docs_search_repo_names_$SUFFIX
+            WHERE %s LIMIT 1
+        ),
+        matching_tags AS (
+            SELECT id, tags
+            FROM lsif_data_docs_search_tags_$SUFFIX
+            WHERE %s LIMIT 1
+        )
+    SELECT
+		lsif_data_docs_search_$SUFFIX.id::bigint,
+        repo_id,
+        path_id,
+        detail,
+        search_key,
+        label,
+        lang_name,
+        repo_name,
+        tags,
+		%s AS search_key_rank,
+		%s AS search_key_reverse_rank,
+		%s AS label_rank,
+		%s AS label_reverse_rank
+    FROM lsif_data_docs_search_$SUFFIX
+    JOIN lsif_data_docs_search_lang_names_$SUFFIX ON lsif_data_docs_search_lang_names_$SUFFIX.id = lang_name_id
+    JOIN lsif_data_docs_search_repo_names_$SUFFIX ON lsif_data_docs_search_repo_names_$SUFFIX.id = repo_name_id
+    JOIN lsif_data_docs_search_tags_$SUFFIX ON lsif_data_docs_search_tags_$SUFFIX.id = tags_id
+    WHERE
+        (%s)
+
+		-- If we found matching lang names, filter to just those.
+        AND (CASE WHEN (SELECT COUNT(*) FROM matching_lang_names) > 0 THEN
+            lang_name_id = ANY(array(SELECT id FROM matching_lang_names))
+        ELSE lang_name_id IS NOT NULL END)
+
+		-- If we found matching repo names, filter to just those.
+        AND (CASE WHEN (SELECT COUNT(*) FROM matching_repo_names) > 0 THEN
+            repo_name_id = ANY(array(SELECT id FROM matching_repo_names))
+        ELSE repo_name_id IS NOT NULL END)
+
+		-- If we found matching tags, filter to just those.
+		AND (CASE WHEN (SELECT COUNT(*) FROM matching_tags) > 0 THEN
+            tags_id = ANY(array(SELECT id FROM matching_tags))
+        ELSE tags_id IS NOT NULL END)
+
+	-- maximum candidates we'll consider: the more candidates we have the better ranking we get,
+	-- but the slower searching is. See https://about.sourcegraph.com/blog/postgres-text-search-balancing-query-time-and-relevancy/
+	-- 10,000 is an arbitrary choice based on current Sourcegraph.com corpus size and performance,
+	-- it'll be tuned as we scale to more repos if perf gets worse or we find we need better
+	-- relevance.
+    LIMIT 10000
+) sub
+ORDER BY
+	-- First rank by search keys, as those are ideally super specific if you write the correct
+	-- format.
+    GREATEST(search_key_rank, search_key_reverse_rank) DESC,
+
+	-- Secondarily rank by label, e.g. function signature. These contain less specific info and
+	-- due to e.g. containing arguments a function takes, have higher chance of collision with the
+	-- desired symbol result, producing a bad match.
+    GREATEST(label_rank, label_reverse_rank) DESC,
+
+	-- If all else failed, sort by something reasonable and deterministic.
+	repo_name DESC,
+	tags DESC,
+	id DESC
+LIMIT %s -- Maximum results we'll actually return.
+`
+
+// scanDocumentationSearchResults reads the documentation search results rows. If no rows matched, (nil, nil) is returned.
+func (s *Store) scanDocumentationSearchResults(rows *sql.Rows, queryErr error) (_ []precise.DocumentationSearchResult, err error) {
+	if queryErr != nil {
+		return nil, queryErr
+	}
+	defer func() { err = basestore.CloseRows(rows, err) }()
+
+	var results []precise.DocumentationSearchResult
+	for rows.Next() {
+		var (
+			result                   precise.DocumentationSearchResult
+			tags                     string
+			searchKeyRank, searchKeyReverseRank, labelRank, labelReverseRank float32
+		)
+		if err := rows.Scan(
+			&result.ID,
+			&result.RepoID,
+			&result.PathID,
+			&result.Detail,
+			&result.SearchKey,
+			&result.Label,
+			&result.Lang,
+			&result.RepoName,
+			&tags,
+			&searchKeyRank,
+			&searchKeyReverseRank,
+			&labelRank,
+			&labelReverseRank,
+		); err != nil {
+			return nil, err
+		}
+		result.Tags = strings.Fields(tags)
+		results = append(results, result)
+	}
+	return results, nil
 }
